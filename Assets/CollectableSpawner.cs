b40b145af4d4e2861d501548b441d1e5a7dac0ff
Ryan Mazzolini <creative630@gmail.com>
2016-02-21 17:31:10 +0200

    Initial scene commit.

---
+++ Assets/CollectableSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectableSpawner : MonoBehaviour 
+{
+	public GameObject CollectablePrefab;
+	public float SpawnRadius;
+	public int NumberOfCollectables;
+	// Use this for initialization
+	void OnEnable () 
+	{		
+		for(int i = 0; i<NumberOfCollectables; i++)
+		{
+			Vector3 spawnPosition = transform.position;
+			spawnPosition += new Vector3 (Mathf.Sin((Mathf.PI*2 /NumberOfCollectables)*i ),0, Mathf.Cos((Mathf.PI*2 /NumberOfCollectables)*i)) * SpawnRadius;
+			Instantiate(CollectablePrefab,spawnPosition,transform.rotation);
+		}
+	}
+}
---
+++ Assets/ParticleDeath.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleDeath : MonoBehaviour
+{
+    private ParticleSystem particleSystem;
+	// Use this for initialization
+	void Start ()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (particleSystem.isStopped)
+	    {
+	        Destroy(gameObject);
+	    }
+	}
+}
